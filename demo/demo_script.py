7eb8a29913992ba1467a8840ae7e57c84e6a37a2
Lars Maxfield <maxfield.lars@gmail.com>
2024-12-02 13:04:17 +0100

    Fix #5 by adding `save_images`, adding `get_image_bytes`, handling image bytes in `_parse_field`, and adding an example in demo script with a PDZ that contains images

--- demo/demo_script.py
+++ demo/demo_script.py
@@ -15,7 +15,8 @@ def main():
         os.path.join(demo_dir, 'pdz24_example.pdz'),
         os.path.join(demo_dir, 'pdz24_example_2.pdz'),
         os.path.join(demo_dir, 'pdz25_example.pdz'),
-        os.path.join(demo_dir, 'pdz25_example_2.pdz')
+        os.path.join(demo_dir, 'pdz25_example_2.pdz'),
+        os.path.join(demo_dir, 'pdz25_example_images.pdz')
     ]
 
     # Process each PDZ file
@@ -34,13 +35,22 @@ def main():
             parsed_pdz = pdz_tool.parse()  # dict with parsed pdz data
 
             parsed_json = pdz_tool.to_json()
-            print(f"Parsed pdz JSON: {parsed_json[0:100]}...")
-            print("Saving JSON...")
-            pdz_tool.save_json(output_dir=demo_dir + "/output")
+            if parsed_json:
+                print(f"Parsed pdz JSON: {parsed_json[0:100]}...")
+                print("Saving JSON...")
+                pdz_tool.save_json(output_dir=demo_dir + "/output")
 
             print("Saving CSV for XRF Spectrum...")
             print(f"Possible Record Names: {pdz_tool.record_names}")
             pdz_tool.save_csv(output_dir=demo_dir + "/output")
+
+            image_record = parsed_pdz.get('Image Details', 0)
+            if image_record:
+                print(f"Saving {image_record['num_images']} images as JPEGs...")
+                pdz_tool.save_images(
+                    output_dir=demo_dir + "/output",
+                    output_suffix='-')  # filename-0.jpeg, filename-1.jpeg, etc.
+
         except Exception as e:
             print(f"An error occurred while processing {pdz_file}: {e}")
             if debug_mode:
